711b58375fda94d7d7463d95cba214cefd7b5d5f
seoyoooooon <24-1215@sonline20.sen.go.kr>
2025-07-17 09:47:37 +0900

    Update 01_인구2025년5월지도2.py

--- pages/01_인구2025년5월지도2.py
+++ pages/01_인구2025년5월지도2.py
@@ -1,1 +1,8 @@
-pip install streamlit folium streamlit-folium
+import pandas as pd
+
+try:
+    df = pd.read_csv("202505_202505_연령별인구현황_월간.csv", encoding="euc-kr")
+    print("✅ 파일 로드 성공!")
+    print(df.head())
+except Exception as e:
+    print("❌ CSV 읽기 실패:", e)
